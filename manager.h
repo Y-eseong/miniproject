9adfea3d80f45368b64a70a68bafb8cde8399f76
김예성 <s21900149@walab-HGU>
2022-04-15 20:07:44 +0900

    manager.h 추가

---
+++ manager.h
@@ -0,0 +1,6 @@
+void searchWho(Product *p, int count);
+void searchAddress(Product *p, int count);
+void searchHow(Product *p, int count);
+int selectNo(Product *p, int count);
+void saveData(Product *p, int count);
+int loadData(Product *p);
